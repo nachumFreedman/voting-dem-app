c497f4af97f01925999f58d29e01c47b39a604b5
nachumFreedman <nochum09@gmail.com>
2017-06-11 14:10:03 +0300

    trying to get npm run start working

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import server from 'socket.io';
+
+export default function startServer() {
+  const io = new Server().attach(8090)
+
+  store.subscribe(
+    () => io.emit('state', store.getState(.toJS()))
+  );
+}
